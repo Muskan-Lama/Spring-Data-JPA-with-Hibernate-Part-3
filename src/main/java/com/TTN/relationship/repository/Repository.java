89d5ee5f8fa73813e62ca87bfe99f2323e2aaa9d
Muskan <muskan.lama@tothenew.com>
2022-11-01 10:07:59 +0530

    done

---
+++ src/main/java/com/TTN/relationship/repository/Repository.java
@@ -0,0 +1,13 @@
+package com.TTN.relationship.repository;
+
+import com.TTN.relationship.entity.Author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repository extends CrudRepository<Author,Integer> {
+
+    //Introduce a List of subjects for author.
+
+
+}
